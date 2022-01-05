c2337cd4ede94779316c1139e72c9e7ecf8b5db2
Sima Nerush <2002ssn@gmail.com>
2022-01-04 21:43:35 -0800

    Add stack implementation

--- 04-stacks/projects/starter/Stacks.playground/Contents.swift
+++ 04-stacks/projects/starter/Stacks.playground/Contents.swift
@@ -1,3 +1,76 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public struct Stack<Element> {
+    private var storage: [Element] = []
+    
+    public init() { }
+    
+    public init(_ elements: [Element]) {
+        storage = elements
+    }
+    
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    public mutating func pop() -> Element? {
+        storage.popLast()
+    }
+    
+    public func peek() -> Element? {
+        storage.last
+    }
+    
+    public var isEmpty: Bool {
+        peek() == nil
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    // Create an array that maps the elements to String via storage.map
+    // Create a new array that reverses the previous array
+    // Flattening the array into a string
+    public var description: String {
+        """
+        ----top----
+        \(storage.map { "\($0)" }.reversed().joined(separator: "\n"))
+        -----------
+        """
+    }
+}
+
+// Make Stack initializable from an array literal
+extension Stack: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
+
+example(of: "using a stack") {
+    var stack = Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    
+    print(stack)
+    
+    if let poppedElement = stack.pop() {
+        assert(4 == poppedElement)
+        print("Popped: \(poppedElement)")
+    }
+}
+
+example(of: "initializing a stack from an array") {
+    let array = ["A", "B", "C", "D"]
+    var stack = Stack(array)
+    stack.pop()
+    print(stack)
+}
+
+example(of: "initializing a stack from array literal") {
+    let stack: Stack = [1.0, 2.0, 3.0, 4.0]
+    print(stack)
+}
