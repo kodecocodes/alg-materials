95d7ebb610c962a074d4b8f1dc210d9b3878b212
Steven Van Impe <steven.vanimpe@icloud.com>
2024-12-18 11:40:26 +0100

    Bump copyright year to 2025

--- 34-quicksort/projects/final/quicksort.playground/Contents.swift
+++ 34-quicksort/projects/final/quicksort.playground/Contents.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Razeware LLC
+// Copyright (c) 2025 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
 var list = [12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8]
